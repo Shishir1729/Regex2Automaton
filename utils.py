fddf74b39004f2a75f422590297da546bf16141a
shishir1729 <shishirr@iisc.ac.in>
2024-03-06 18:52:52 +0530

    Commented code

--- utils.py
+++ utils.py
@@ -87,99 +87,157 @@ class ETree:
     ###################################################################
     # .
     def operatorDot(self, fsaX, fsaY):
+        # The number of states in the new FSA is the sum of the states in fsaX and fsaY
         numStates = fsaX.numStates + fsaY.numStates
+
+        # The start states of the new FSA are the start states of fsaX
         startStates = fsaX.startStates
+
+        # The final states of the new FSA are the final states of fsaY, shifted by the number of states in fsaX
         finalStates = [state+fsaX.numStates for state in fsaY.finalStates]
+
+        # Initialize the transition table for the new FSA
         alphabetTransitions = {}
+
+        # For each symbol in the alphabet
         for symbol in 'abc':
             alphabetTransitions[symbol] = []
+
+            # Initialize the transition table for this symbol
             for state in range(numStates):
                 alphabetTransitions[symbol].append([0]*numStates)
+
+            # Copy the transitions from fsaX for this symbol
             for state in range(fsaX.numStates):
                 alphabetTransitions[symbol][state] = fsaX.alphabetTransitions[symbol][state]
                 alphabetTransitions[symbol][state].extend([0]*(fsaY.numStates))
+
+            # For each state in fsaX, if it transitions to a final state on this symbol, add a transition to all start states in fsaY
             for state in range(fsaX.numStates):
                 for i in range(fsaX.numStates):
                     if fsaX.alphabetTransitions[symbol][state][i] == 1 and i in fsaX.finalStates:
                         for j in range(fsaX.numStates, numStates):
                             if j - fsaX.numStates in fsaY.startStates:
                                 alphabetTransitions[symbol][state][j] = 1
+
+            # Copy the transitions from fsaY for this symbol, shifted by the number of states in fsaX
             for state in range(fsaX.numStates, numStates):
                 alphabetTransitions[symbol][state] = [0] * fsaX.numStates + fsaY.alphabetTransitions[symbol][state-fsaX.numStates]
-        
+
+        # If any of the start states in fsaX are also final states, add the start states of fsaY as start states
         for state in fsaX.startStates:
             if state in fsaX.finalStates:
                 startStates.extend([state + fsaX.numStates for state in fsaY.startStates])
                 break
 
+        # Add transitions on the empty string from each state to itself
         alphabetTransitions['e'] = [[1 if i == j else 0 for j in range(numStates)] for i in range(numStates)]
-        
-        fsa = NFA(numStates=numStates, startStates=startStates, finalStates=finalStates, alphabetTransitions=alphabetTransitions)
-        return fsa
 
-        
+        # Create the new FSA
+        fsa = NFA(numStates=numStates, startStates=startStates, finalStates=finalStates, alphabetTransitions=alphabetTransitions)
+
+        return fsa
+            
 
     # +
     def operatorPlus(self, fsaX, fsaY):
-        #add fsaX.numstates to all states of fsaY
+        # The number of states in the new FSA is the sum of the states in fsaX and fsaY
         fsaY.numStates = fsaX.numStates + fsaY.numStates
+
+        # The start states of the new FSA are the start states of fsaY, shifted by the number of states in fsaX (We will add the start states of fsaX later)
         fsaY.startStates = [state+fsaX.numStates for state in fsaY.startStates]
+
+        # The final states of the new FSA are the final states of fsaY, shifted by the number of states in fsaX (We will add the final states of fsaX later)
         fsaY.finalStates = [state+fsaX.numStates for state in fsaY.finalStates]
+
+        # For each symbol in the alphabet
         for symbol in 'abc':
+            # The transitions for this symbol in the new FSA are the transitions in fsaX followed by the transitions in fsaY
             fsaY.alphabetTransitions[symbol] = fsaX.alphabetTransitions[symbol] + fsaY.alphabetTransitions[symbol]
+
+            # For each state in fsaY
             for state in range(fsaX.numStates, fsaY.numStates):
-                #Add 0s in the beginning for the new states
+                # Add zeros at the beginning of the transition list for this state, to account for the new states from fsaX
                 fsaY.alphabetTransitions[symbol][state] = [0]*fsaX.numStates + fsaY.alphabetTransitions[symbol][state]
 
+            # For each state in fsaX
             for state in range(fsaX.numStates):
+                # Copy the transitions from fsaX for this symbol
                 fsaY.alphabetTransitions[symbol][state] = fsaX.alphabetTransitions[symbol][state]
+                # Extend the transition list for this state with zeros, to account for the new states from fsaY
                 fsaY.alphabetTransitions[symbol][state].extend([0]*(fsaY.numStates-fsaX.numStates))
         
+        # Add transitions on the empty string from each state to itself
         fsaY.alphabetTransitions['e'] = [[1 if i == j else 0 for j in range(fsaY.numStates)] for i in range(fsaY.numStates)]
 
+        # The start states of the new FSA are the start states of both fsaX and fsaY
         fsaY.startStates = fsaX.startStates + fsaY.startStates
+
+        # The final states of the new FSA are the final states of both fsaX and fsaY
         fsaY.finalStates = fsaX.finalStates + fsaY.finalStates
+
+        # Return the new FSA
         return fsaY
+    
     
     # *
     def operatorStar(self, fsaX):
+        # For each symbol in the alphabet
         for symbol in 'abc':
+            # Add a new state to the FSA
             fsaX.alphabetTransitions[symbol].append([0]*(fsaX.numStates+1))
+
             for state in range(fsaX.numStates):
+                # Extend the transition list for this state with a zero, to account for the new state
                 fsaX.alphabetTransitions[symbol][state].append(0)
-            
+
+            # For each state in the FSA
             for state in range(fsaX.numStates):
+                #If the state is a start state
                 if state in fsaX.startStates:
+                    # For each state in the FSA
                     for i in range(fsaX.numStates):
+                        # If there is a transition from the current state to state i on this symbol
                         if fsaX.alphabetTransitions[symbol][state][i] == 1:
+                            # Add a transition from the new state to state i on this symbol
                             fsaX.alphabetTransitions[symbol][fsaX.numStates][i] = 1
+                # For each state in the FSA
                 for i in range(fsaX.numStates):
+                    # If there is a transition from the current state to state i on this symbol, and state i is a final state
                     if fsaX.alphabetTransitions[symbol][state][i] == 1 and i in fsaX.finalStates:
+                        # Add a transition from the current state to the new state on this symbol
                         fsaX.alphabetTransitions[symbol][state][fsaX.numStates] = 1
 
-            
-            #Decide self loop
-            for state in range(fsaX.numStates):
-                if state in fsaX.startStates:
-                    for i in range(fsaX.numStates):
-                        if fsaX.alphabetTransitions[symbol][state][i] == 1 and i in fsaX.finalStates:
-                            fsaX.alphabetTransitions[symbol][fsaX.numStates][fsaX.numStates] = 1
-            
+            # For each start state in the FSA
+            for state in fsaX.startStates:
+                # For each state in the FSA
+                for i in range(fsaX.numStates):
+                    # If there is a transition from the current state to state i on this symbol where i is a final state.
+                    if fsaX.alphabetTransitions[symbol][state][i] == 1 and i in fsaX.finalStates:
+                        # Add a self-loop at the new state on this symbol
+                        fsaX.alphabetTransitions[symbol][fsaX.numStates][fsaX.numStates] = 1
+
+        # Add transitions on the empty string from each state to itself
         fsaX.alphabetTransitions['e'] = [[1 if i == j else 0 for j in range(fsaX.numStates+1)] for i in range(fsaX.numStates+1)]
 
+        #The new state is the only start and final state
+        fsaX.finalStates = [fsaX.numStates]
+        fsaX.startStates = [fsaX.numStates]
 
-            
+        # Increase the number of states in the FSA
         fsaX.numStates += 1
-        fsaX.startStates = [fsaX.numStates-1]
-        fsaX.finalStates = [fsaX.numStates-1]
 
+        # Return the modified FSA
         return fsaX
 
     # a, b, c and e for epsilon
     def alphabet(self, symbol):
+        # Create an FSA with a single state and no transitions
         if symbol == 'e':
             fsa = NFA(numStates=1, startStates=[0], finalStates=[0], alphabetTransitions={'a': [[0]], 'b': [[0]], 'c': [[0]], 'e': [[1]]})
             return fsa
+        
+        # Create an FSA with two states and transitions for the given symbol
         elif symbol == 'a':
             fsa = NFA(numStates=2, startStates=[0], finalStates=[1], alphabetTransitions={'a': [[0,1], [0,0]], 'b': [[0,0], [0,0]], 'c': [[0,0], [0,0]], 'e': [[1,0], [0,1]]})
         elif symbol == 'b':
@@ -224,3 +282,4 @@ class ETree:
             return fsa
 
     ######################################################################
+        
